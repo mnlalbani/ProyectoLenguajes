165cd78c83264b1f86ad210c6589f8916f42ea63
mnlalbani <mnlalbani@gmail.com>
2016-04-27 12:59:48 -0430

    actualizado sql

--- reserva_estadio.sql
+++ reserva_estadio.sql
@@ -1,59 +1,59 @@
-/*Cliente: ced(c), nombre, apellido, telefono, correo, direccion.
-Ticket: codigo(c), fecha, ced(cliente), asiento (asiento)
-Asiento: codigo(c), estatus, zona(zona)
-Zona: codigo (c), nombre, cant asientos, estadio(estadio)
-Estadio: codigo(c), nombre
-Juego: codigo(c), equipo a, equipo b, fecha del juego, hora, estadio(estadio)
-*/
-DROP Database IF EXISTS reserva_estadio;
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2016 at 07:26 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
-CREATE Database reserva_estadio;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE TABLE `reserva_estadio`.`cliente`  (
-  `cedula` int(8) NOT NULL,
-  `nombre` varchar NOT NULL,
-  `apellido` varchar NOT NULL,
-  `telefono` int(8) NOT NULL,
-  `correo` varchar NOT NULL,
-  `direccion` varchar NOT NULL,
-  PRIMARY KEY (`cedula`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
-CREATE TABLE `reserva_estadio`.`ticket`{
-	`codigo` varchar NOT NULL,
-	`fecha`	varchar NOT NULL,
-	`cedula` varchar NOT NULL,
-	`asiento` varchar NOT NULL,
-	PRIMARY KEY(`codigo`)
-} ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-CREATE TABLE `reserva_estadio`.`asiento`{
-	`codigo` varchar NOT NULL,
-	`estatus` varchar NOT NULL,
-	`zona` varchar NOT NULL,
-	PRIMARY KEY(`codigo`)
-} ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+--
+-- Database: `reserva_estadio`
+--
 
-CREATE TABLE `reserva_estadio`.`zona`{
-	`codigo` varchar NOT NULL,
-	`nombre`	varchar NOT NULL,
-	`cantidad_asiento` int NOT NULL,
-	`estadio` varchar NOT NULL,
-	PRIMARY KEY(`codigo`)
-} ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 
-CREATE TABLE `reserva_estadio`.`estadio`{
-	`codigo` varchar NOT NULL,
-	`nombre` varchar NOT NULL,
-	PRIMARY KEY(`codigo`)
-} ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `ticket`
+--
 
-CREATE TABLE `reserva_estadio`.`juego`{
-	`codigo` varchar NOT NULL,
-	`equipo_a` varchar NOT NULL,
-	`equipo_b` varchar NOT NULL,
-	`fecha`	varchar NOT NULL,
-	`hora` varchar NOT NULL,
-	`estadio` varchar NOT NULL,
-	PRIMARY KEY(`codigo`)
-} ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `ticketid` varchar(20) NOT NULL,
+  `equipo_1` varchar(30) NOT NULL,
+  `equipo_2` varchar(30) NOT NULL,
+  `hora` varchar(10) NOT NULL,
+  `lugar` varchar(50) NOT NULL,
+  `valor` int(10) NOT NULL,
+  `zonaElegida` varchar(10) NOT NULL,
+  `asientoElegido` varchar(10) NOT NULL,
+  `cedula` int(10) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `confirmado` varchar(3) NOT NULL,
+  PRIMARY KEY (`ticketid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`ticketid`, `equipo_1`, `equipo_2`, `hora`, `lugar`, `valor`, `zonaElegida`, `asientoElegido`, `cedula`, `nombre`, `apellido`, `telefono`, `correo`, `confirmado`) VALUES
+('', 'Aguilas del Zulia', 'Cardenales de Lara', '06:00pm', 'Luis Aparicio El Grande', 200, '', '', 544564, 'asdasd', 'asd', 123412, 'asdasd@g.com', ''),
+('grada5', 'Aguilas del Zulia', 'Cardenales de Lara', '06:00pm', 'Luis Aparicio El Grande', 200, 'grada', '5', 2312, 'asdas', 'dasdas', 12312, 'sdaasd@co.com', ''),
+('grada6', 'Aguilas del Zulia', 'Cardenales de Lara', '06:00pm', 'Luis Aparicio El Grande', 200, 'grada', '6', 23423, 'asdasd', 'dasda', 1231231, 'sdfsdsd@sdfs.com', ''),
+('grada8', 'Aguilas del Zulia', 'Cardenales de Lara', '06:00pm', 'Luis Aparicio El Grande', 200, 'grada', '8', 23423, 'asdasd', 'asdas', 234234, 'asdasd@gmail.cvom', '');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
