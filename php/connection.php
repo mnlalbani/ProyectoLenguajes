4d74afb21cf3741ac99b3e8eaae88ddcc832158c
Manuel Albani <mnlalbani@gmail.com>
2017-04-19 15:08:14 -0400

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,5 @@
 <?php 
-	$mysqli	= new mysqli("localhost" , "root", "", "reserva_estadio");
+	$mysqli	= new mysqli("127.0.0.1" , "root", "", "reserva_estadio");
 	if ($mysqli->connect_errno){
 		echo "<p>Coneccion MySQL fallida: (".$mysqli->connect_errno .")".$mysqli->connect_error ."</p>";
 		echo $mysqli-host_info ."\n";
